56a6bff3fd6fe6c992e2a4e88df095bf8f0d4ffc
KhaMinh <minhkha06122004@gmail.com>
2025-06-20 16:09:20 +0700

    update

--- backend/src/routes/post-router.js
+++ backend/src/routes/post-router.js
@@ -1,6 +1,7 @@
 // routes/postRoutes.js
 import express from 'express';
 import authMiddleware from "../middlewares/auth-middlewares.js";
+import upload from "../middlewares/uploadMiddelware.js"
 import {
   createPost,
   getAllPosts,
@@ -13,12 +14,12 @@ import {
 import { toggleLike, getLikes } from '../controllers/like-controller.js';
 const router = express.Router();
 
-router.post('/',authMiddleware, createPost);
+router.post('/',authMiddleware,upload.single('image'), createPost);
 router.get('/', getAllPosts);
 router.post('/like', authMiddleware, toggleLike);
 router.get('/like', getLikes);
 router.get('/my', authMiddleware, getMyPosts);
 router.get('/:id', getPostById);
-router.put('/:id', authMiddleware,updatePost);
+router.put('/:id', authMiddleware,upload.single('image'),updatePost);
 router.delete('/:id',authMiddleware, deletePost);
 export default router;
